f1f8fe788bf7c3926eb610cd042536f55afbc443
PolinaEfimova888 <57696453+PolinaEfimova888@users.noreply.github.com>
2020-10-21 14:25:34 +0800

    Create register.py

---
+++ register.py
@@ -0,0 +1,28 @@
+import os
+import re
+from urllib.parse import urlparse
+ 
+array = []
+
+register_file = open('register.txt')
+lines = register_file.readlines()
+
+for i in range(1, len(lines)):
+    b = lines[i].strip().split(';')
+    array.extend(b)
+ 
+url = input("Enter URL: ")
+if url in array:
+    print("URL is blocked")
+else:
+    print("URL insn't bloked")
+ 
+parse = urlparse(url)
+domain = parse.netloc.split(".")[1:]
+server = ".".join(domain)
+print("DOMAIN:",server)
+ 
+if server in array:
+    print("In the list")
+else:
+    print("Isn't in the list")
